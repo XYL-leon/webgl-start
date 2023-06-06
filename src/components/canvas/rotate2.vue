07b8ba8d1e0311f69592393a8dff04d0af7c0c20
xuyunlong <xuyunlong921776@cma.cn>
2023-06-06 14:03:42 +0800

    4个象限的rotate

--- src/components/canvas/rotate2.vue
+++ src/components/canvas/rotate2.vue
@@ -4,12 +4,22 @@ import { ref, onMounted } from "vue";
 let container = ref(null);
 var canvas, ctx;
 let angle_1 = ref(0);
+let angle_2 = ref(0);
+
+let angle_3 = ref(0);
+let angle_4 = ref(0);
+
+let angle_5 = ref(0);
+let angle_6 = ref(0);
+
+let angle_7 = ref(0);
+let angle_8 = ref(0);
 
 function init() {
   canvas = container.value;
   ctx = canvas.getContext("2d");
 }
-function draw() {
+function draw(angle) {
   //初始的样式（绘制状态）并绘制矩形
   ctx.shadowOffsetX = 5;
   ctx.shadowOffsetY = 5;
@@ -18,24 +28,24 @@ function draw() {
 
   ctx.save(); // 1 保存正常的坐标系
   ctx.translate(100, 100); // 2 改变坐标系位置
-  ctx.rotate((angle_1.value * Math.PI) / 180); // 3 旋转坐标系
+  ctx.rotate((angle * Math.PI) / 180); // 3 旋转坐标系
   ctx.fillStyle = "red";
   ctx.font = "20px Arial";
   ctx.textBaseline = "middle";
   ctx.textAlign = "center";
-  ctx.fillText("你好，世界！" + angle_1.value, 0, 0);
+  ctx.fillText("你好，世界！" + angle, 0, 0);
   ctx.restore(); // 4 从栈中取出之前保存的状态
 }
-function drawLine(pointA, pointB) {
-  ctx.beginPath();
-  // ctx.moveTo(Math.floor(pointA[0]), Math.floor(pointA[1])); // 将笔触移动到该点
-  // ctx.lineTo(Math.floor(pointB[0]), Math.floor(pointB[1]));
-  console.log(Math.floor(pointA[0] * 10), Math.floor(pointA[1] * 10));
-  console.log(Math.floor(pointB[0] * 10), Math.floor(pointB[1] * 10));
-  ctx.moveTo(828, 634); // 将笔触移动到该点
-  ctx.lineTo(832, 634);
-  ctx.fill();
-}
+// function drawLine(pointA, pointB) {
+//   ctx.beginPath();
+//   // ctx.moveTo(Math.floor(pointA[0]), Math.floor(pointA[1])); // 将笔触移动到该点
+//   // ctx.lineTo(Math.floor(pointB[0]), Math.floor(pointB[1]));
+//   // console.log(Math.floor(pointA[0] * 10), Math.floor(pointA[1] * 10));
+//   // console.log(Math.floor(pointB[0] * 10), Math.floor(pointB[1] * 10));
+//   ctx.moveTo(pointA[0], pointA[1]); // 将笔触移动到该点
+//   ctx.lineTo(pointB[0], pointB[1]);
+//   ctx.fill();
+// }
 function computeAngle(pointA, pointB) {
   console.log("-------------");
   let angleDeg =
@@ -59,22 +69,103 @@ onMounted(() => {
    * */
   // 第一象限 降序 —_
   let bbb = [
-    [82.8, 63.41665],
-    [83.2, 63.41303],
+    // 45
+    [0, 30],
+    [30, 0],
   ];
+  // 升序
+  // let ccc = [
+  //   // -45
+  //   [0, 0],
+  //   [30, 30],
+  // ];
+  let ccc = [
+    [97.69617, 48.6],
+    [97.8, 48.65742],
+  ];
+  // 第二象限 降序
+  let ddd = [
+    // 45
+    // 45
+    [-30, 30],
+    [0, 0],
+  ];
+  // 升序
+  let eee = [
+    // -45
+    [-30, 0],
+    [0, 30],
+  ];
+  // 第三象限 降序
+  let fff = [
+    // 45
+    [-30, 0],
+    [0, -30],
+  ];
+  // 升序
+  let ggg = [
+    // -45
+    [-30, -30],
+    [0, 0],
+  ];
+  // 第四象限 降序
+  let hhh = [
+    // 45
+    [0, 0],
+    [30, -30],
+  ];
+  // 升序 //-45
+  let iii = [
+    [0, -30],
+    [30, 0],
+  ];
+
   // angle_1.value = computeAngle(aaa[0], aaa[1]);
   angle_1.value = computeAngle(bbb[0], bbb[1]);
-  draw();
-  drawLine(bbb[0], bbb[1]);
+  angle_2.value = computeAngle(ccc[0], ccc[1]);
+
+  angle_3.value = computeAngle(ddd[0], ddd[1]);
+  angle_4.value = computeAngle(eee[0], eee[1]);
+
+  angle_5.value = computeAngle(fff[0], fff[1]);
+  angle_6.value = computeAngle(ggg[0], ggg[1]);
+
+  angle_7.value = computeAngle(hhh[0], hhh[1]);
+  angle_8.value = computeAngle(iii[0], iii[1]);
+
+  draw(angle_8.value);
+  // drawLine(bbb[0], bbb[1]);
+  // drawLine(hhh[0], hhh[1]);
 });
 </script>
 
 <template>
   <canvas ref="container" width="900" height="900"></canvas>
   <ul>
-    <li :style="'transform:rotate(' + angle_1 + 'deg)'">
+    <!-- <li :style="'transform:rotate(' + angle_1 + 'deg);background:#ff0000;'">
       旋转角度{{ angle_1 }}
+    </li> -->
+    <li :style="'transform:rotate(' + angle_2 + 'deg);background:#ff0000;'">
+      旋转角度{{ angle_2 }}
     </li>
+    <!-- <li :style="'transform:rotate(' + angle_3 + 'deg);background:#fff000;'">
+      旋转角度{{ angle_3 }}
+    </li>
+    <li :style="'transform:rotate(' + angle_4 + 'deg);background:#fff000;'">
+      旋转角度{{ angle_4 }}
+    </li>
+    <li :style="'transform:rotate(' + angle_5 + 'deg);background:#00ff00;'">
+      旋转角度{{ angle_5 }}
+    </li>
+    <li :style="'transform:rotate(' + angle_6 + 'deg);background:#00ff00;'">
+      旋转角度{{ angle_6 }}
+    </li>
+    <li :style="'transform:rotate(' + angle_7 + 'deg);background:#00ffff;'">
+      旋转角度{{ angle_7 }}
+    </li>
+    <li :style="'transform:rotate(' + angle_8 + 'deg);background:#00ffff;'">
+      旋转角度{{ angle_8 }}
+    </li> -->
   </ul>
 </template>
 
