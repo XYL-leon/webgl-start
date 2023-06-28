148174e150388e34d9ec97a7e2620b40b7b50676
xuyunlong <xuyunlong921776@cma.cn>
2023-06-28 18:56:55 +0800

    绘制圆角矩形

--- src/components/canvas/port.vue
+++ src/components/canvas/port.vue
@@ -4,87 +4,82 @@ let container = ref(null);
 let ctx = null;
 
 function drawPort(label, lineColor, iconBG) {
-  var canvas = container.value;
-  if (canvas.getContext) {
-    ctx = canvas.getContext("2d");
+  ctx.font = "12px Arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  const textMetrics = ctx.measureText(label);
+  const textWidth =
+    Math.ceil(
+      textMetrics.actualBoundingBoxRight + textMetrics.actualBoundingBoxLeft
+    ) + 10;
 
-    ctx.font = "12px Arial";
-    ctx.textBaseline = "middle";
-    ctx.textAlign = "center";
-    const textMetrics = ctx.measureText(label);
-    const textWidth =
-      Math.ceil(
-        textMetrics.actualBoundingBoxRight + textMetrics.actualBoundingBoxLeft
-      ) + 10;
+  console.log(textWidth);
+  // const width = textWidth < 58 ? 58 : textWidth;
+  const width = 108;
+  const height = 32;
+  const margin = 5;
+  const radius = height / 2;
+  ctx.fillStyle = iconBG;
 
-    console.log(textWidth);
-    // const width = textWidth < 58 ? 58 : textWidth;
-    const width = 108;
-    const height = 32;
-    const margin = 5;
-    const radius = height / 2;
-    ctx.fillStyle = iconBG;
+  ctx.beginPath();
+  ctx.strokeStyle = "rgba(255, 255, 255, 1)";
+  ctx.lineWidth = 32;
+  ctx.lineJoin = "round";
+  ctx.moveTo(margin + radius, height / 2 + margin);
+  ctx.lineTo(width + margin + radius, height / 2 + margin);
+  ctx.closePath();
+  ctx.stroke();
 
-    ctx.beginPath();
-    ctx.strokeStyle = "rgba(255, 255, 255, 1)";
-    ctx.lineWidth = 32;
-    ctx.lineJoin = "round";
-    ctx.moveTo(margin + radius, height / 2 + margin);
-    ctx.lineTo(width + margin + radius, height / 2 + margin);
-    ctx.closePath();
-    ctx.stroke();
+  // 上横线
+  ctx.lineWidth = 1;
+  ctx.strokeStyle = lineColor;
 
-    // 上横线
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = lineColor;
+  ctx.beginPath();
+  ctx.moveTo(margin + radius, margin);
+  ctx.lineTo(width + margin + radius, margin);
+  ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(margin + radius, margin);
-    ctx.lineTo(width + margin + radius, margin);
-    ctx.stroke();
+  // 下横线加三角
+  ctx.beginPath();
+  ctx.moveTo(margin + radius, height + margin);
+  ctx.lineTo(width / 2 + margin - 5 + radius, height + margin);
+  ctx.lineTo((width + margin * 2) / 2 + radius, height + margin + 10);
+  ctx.lineTo(width / 2 + margin + 5 + radius, height + margin);
+  ctx.lineTo(width + margin + radius, height + margin);
+  ctx.stroke();
 
-    // 下横线加三角
-    ctx.beginPath();
-    ctx.moveTo(margin + radius, height + margin);
-    ctx.lineTo(width / 2 + margin - 5 + radius, height + margin);
-    ctx.lineTo((width + margin * 2) / 2 + radius, height + margin + 10);
-    ctx.lineTo(width / 2 + margin + 5 + radius, height + margin);
-    ctx.lineTo(width + margin + radius, height + margin);
-    ctx.stroke();
+  // 左侧半圆
+  ellipse(
+    ctx,
+    margin + radius,
+    height / 2 + margin,
+    radius,
+    radius,
+    Math.PI / 2
+  );
+  // 右侧半圆
+  ellipse(
+    ctx,
+    width + margin + radius,
+    height / 2 + margin,
+    radius,
+    radius,
+    -Math.PI / 2
+  );
 
-    // 左侧半圆
-    ellipse(
-      ctx,
-      margin + radius,
-      height / 2 + margin,
-      radius,
-      radius,
-      Math.PI / 2
-    );
-    // 右侧半圆
-    ellipse(
-      ctx,
-      width + margin + radius,
-      height / 2 + margin,
-      radius,
-      radius,
-      -Math.PI / 2
-    );
+  // icon
+  ctx.beginPath();
+  ctx.arc(margin + radius, height / 2 + margin, 12, 0, 360, false);
+  ctx.fill();
 
-    // icon
-    ctx.beginPath();
-    ctx.arc(margin + radius, height / 2 + margin, 12, 0, 360, false);
-    ctx.fill();
+  // ctx.drawImage(img, 0, 0, 24, 24);
 
-    // ctx.drawImage(img, 0, 0, 24, 24);
-
-    // 文字
-    ctx.fillText(
-      label,
-      width / 2 + margin + radius + 10,
-      height / 2 + margin + 2
-    );
-  }
+  // 文字
+  ctx.fillText(
+    label,
+    width / 2 + margin + radius + 10,
+    height / 2 + margin + 2
+  );
 }
 function ellipse(context, x, y, a, b, rotate) {
   context.save();
@@ -99,27 +94,60 @@ function ellipse(context, x, y, a, b, rotate) {
   context.restore();
   ctx.stroke();
 }
+function drawRoundRect(ctx, x, y, width, height, radius) {
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(255,0,0,0.5)";
+  ctx.arc(x + radius, y + radius, radius, Math.PI, (Math.PI * 3) / 2);
+  ctx.lineTo(width - radius + x, y);
+  ctx.arc(
+    width - radius + x,
+    radius + y,
+    radius,
+    (Math.PI * 3) / 2,
+    Math.PI * 2
+  );
+  ctx.lineTo(width + x, height + y - radius);
+  ctx.arc(
+    width - radius + x,
+    height - radius + y,
+    radius,
+    0,
+    (Math.PI * 1) / 2
+  );
+  ctx.lineTo(radius + x, height + y);
+  ctx.arc(radius + x, height - radius + y, radius, (Math.PI * 1) / 2, Math.PI);
+  ctx.fill();
+  ctx.closePath();
+}
 
 onMounted(() => {
-  const label = "12.4m/s";
-  const lineColor = "rgba(255,0,0,0.5)";
-  const iconBG = "rgba(255,0,0,0.5)";
-  drawPort(label, lineColor, iconBG);
+  var canvas = container.value;
+  ctx = canvas.getContext("2d");
+
+  // const label = "12.4m/s";
+  // const lineColor = "rgba(255,0,0,0.5)";
+  // const iconBG = "rgba(255,0,0,0.5)";
+  // drawPort(label, lineColor, iconBG);
+
+  drawRoundRect(ctx, 3, 3, 50, 16, 5);
 });
 </script>
 
 <template>
-  <canvas id="tutorial" ref="container" width="150" height="52"> </canvas>
+  <!-- <canvas id="tutorial" ref="container" width="150" height="52"> </canvas> -->
+  <canvas id="tutorial" ref="container" width="56" height="22"> </canvas>
 </template>
 <style scoped>
 #tutorial {
   display: block;
   /* border: 1px solid black; */
   border: 0;
-  width: 150px;
-  height: 52px;
+  /* width: 150px;
+  height: 52px; */
+  width: 56px;
+  height: 22px;
   margin: 100px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  background: rgb(77, 228, 72);
+  /* background: rgb(77, 228, 72); */
 }
 </style>
