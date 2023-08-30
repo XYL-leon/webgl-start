b63d46e6df8090339843ed7df4a311f7e56e424a
xuyunlong <xuyunlong921776@cma.cn>
2023-08-30 17:47:20 +0800

    绘制船舶长、宽

--- src/components/canvas/renderFleet.vue
+++ src/components/canvas/renderFleet.vue
@@ -16,16 +16,16 @@ function draw(point, rotate) {
   ctx.beginPath();
   ctx.lineWidth = 0.7;
 
-    // ctx.moveTo(0, -10);
-    // ctx.lineTo(5, 9);
-    // ctx.lineTo(-5, 9);
-    // ctx.lineTo(0, -10);
+  // ctx.moveTo(0, -10);
+  // ctx.lineTo(5, 9);
+  // ctx.lineTo(-5, 9);
+  // ctx.lineTo(0, -10);
 
-    ctx.moveTo(0, -20);
-    ctx.lineTo(0, -10);
-    ctx.lineTo(5, 9);
-    ctx.lineTo(-5, 9);
-    ctx.lineTo(0, -10);
+  ctx.moveTo(0, -20);
+  ctx.lineTo(0, -10);
+  ctx.lineTo(5, 9);
+  ctx.lineTo(-5, 9);
+  ctx.lineTo(0, -10);
 
   // ctx.moveTo(-1, -9);
   // ctx.lineTo(7, -1);
@@ -41,22 +41,89 @@ function draw(point, rotate) {
 
   ctx.restore();
 }
+// 船舶长、宽
+function drawShipBreadthAndLength(point, rotate) {
+  // const breadth = 20;
+  // const length = 136;
+  const breadth = 60;
+  const length = 333;
+  // const breadth = 26;
+  // const length = 108;
+  // const breadth = 32 *2;
+  // const length = 74 *2;
+  ctx.save();
+  ctx.translate(point.x, point.y);
+
+  ctx.rotate((Math.PI / 180) * rotate);
+
+  ctx.beginPath();
+  ctx.lineWidth = 0.7;
+
+  // // 尾部
+  // ctx.moveTo(-breadth / 2, -length / 2);
+  // ctx.lineTo(breadth / 2, -length / 2);
+  // // 首
+  // ctx.lineTo(breadth / 2, length / 2);
+  // ctx.lineTo(-breadth / 2, length / 2);
+
+  /**
+   * 0度向南
+   * */
+  // // 尾部
+  // ctx.moveTo(-breadth / 2, -length / 2);
+  // ctx.lineTo(-(breadth / 4), -(length / 2 + length / 25));
+  // ctx.lineTo(breadth / 4, -(length / 2 + length / 25));
+  // ctx.lineTo(breadth / 2, -length / 2);
+  // // 首
+  // ctx.lineTo(breadth / 2, length / 2);
+  // ctx.lineTo(breadth / 5, length / 2 + length / 10);
+  // ctx.lineTo(0, length / 2 + length / 8);
+  // ctx.lineTo(-breadth / 5, length / 2 + length / 10);
+  // ctx.lineTo(-breadth / 2, length / 2);
+
+  /**
+   * 0度向北
+   * */
+  // 尾部
+  ctx.moveTo(-breadth / 2, length / 2);
+  ctx.lineTo(-(breadth / 4), length / 2 + length / 25);
+  ctx.lineTo(breadth / 4, length / 2 + length / 25);
+  ctx.lineTo(breadth / 2, length / 2);
+  // // 首
+  ctx.lineTo(breadth / 2, -length / 2);
+  ctx.lineTo(breadth / 5, -(length / 2 + length / 10));
+  ctx.lineTo(0, -(length / 2 + length / 8));
+  ctx.lineTo(-breadth / 5, -(length / 2 + length / 10));
+  ctx.lineTo(-breadth / 2, -length / 2);
+
+  ctx.lineTo(-breadth / 2, length / 2);
+
+  ctx.fillStyle = "rgb(255,0,0)";
+  ctx.fill();
+  ctx.strokeStyle = "rgb(20, 24, 26)";
+  ctx.stroke();
+  ctx.closePath();
+
+  ctx.restore();
+}
 
 onMounted(() => {
   init();
-  draw({ x: 150, y: 150 }, 0);
+  // draw({ x: 150, y: 150 }, 0);
+  drawShipBreadthAndLength({ x: 250, y: 250 }, 223);
+  // drawShipBreadthAndLength({ x: 250, y: 250 }, 0);
 });
 </script>
 
 <template>
-  <canvas ref="container" width="300" height="300"></canvas>
+  <canvas ref="container" width="500" height="500"></canvas>
   <div class="box"></div>
 </template>
 
 <style>
 canvas {
-  width: 300px;
-  height: 300px;
+  width: 500px;
+  height: 500px;
   border: 1px solid red;
   margin: 100px;
 }
