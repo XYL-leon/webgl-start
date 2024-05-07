9ccc468ea1084f855a1a845c778decf4c847de11
xuyunlong <xuyunlong921776@cma.cn>
2024-05-07 19:03:27 +0800

    3d box

--- src/components/three/init.vue
+++ src/components/three/init.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, nextTick, ref } from "vue";
+import { onMounted, nextTick, ref, setTransitionHooks } from "vue";
 import * as THREE from "three";
 // 场景 Scene
 // 相机 Camera
@@ -34,22 +34,39 @@ class FirstDemo {
 
     this.scene = new THREE.Scene();
   }
-  render() {
+  render(containerWidth, containerHeight) {
     // this.drawLine();
-    this.draw3DBox();
+    this.draw3DBox(containerWidth, containerHeight);
   }
   clear() {
-    console.log(this.scene);
     this.renderer.clear();
   }
-  changeCamera(x, y, z) {
-    this.camera = new THREE.PerspectiveCamera(30, this.width / this.height, 1, 3000);
+  changeCamera(x, y, z, containerWidth, containerHeight) {
+    this.camera = new THREE.PerspectiveCamera(
+      30, // 摄像机视锥体垂直视野角度
+      containerWidth / containerHeight, // 摄像机视锥体长宽比
+      1, // 摄像机视锥体近端面
+      3000 // 摄像机视锥体远端面
+    );
+    this.renderer.setSize(containerWidth, containerHeight);
 
-    // this.camera.position.set(200, (this.testY += 10), 200);
-    this.testY += 10;
-    if (this.testY > 360) this.testY = 0;
+    if (containerWidth > this.width) {
+      if (this.testY >= 360) {
+        this.testY = 360;
+      } else {
+        this.testY += 10;
+      }
+    } else {
+      if (this.testY <= -360) {
+        this.testY = -360;
+      } else {
+        this.testY -= 10;
+      }
+    }
     this.camera.position.set(this.testY, this.testY, 200);
     this.camera.lookAt(x, y, z);
+    this.width = containerWidth;
+    this.height = containerHeight;
   }
   //create a blue LineBasicMaterial
   drawLine() {
@@ -67,7 +84,9 @@ class FirstDemo {
     this.scene.add(line);
     this.renderer.render(this.scene, this.camera);
   }
-  draw3DBox() {
+  draw3DBox(containerWidth, containerHeight) {
+    const _width = containerWidth ? containerWidth : this.width;
+    const _height = containerHeight ? containerHeight : this.height;
     // 几何体 长方体
     const geometry = new THREE.BoxGeometry(20, 20, 20);
     // 材质
@@ -80,7 +99,7 @@ class FirstDemo {
     this.scene.add(mesh);
 
     // this.changeCamera(mesh.position);
-    this.changeCamera(0, 0, 0);
+    this.changeCamera(0, 0, 0, _width, _height);
 
     this.renderer.render(this.scene, this.camera);
   }
@@ -88,11 +107,9 @@ class FirstDemo {
 
 function listeningToScreenSize(firstDemo) {
   window.addEventListener("resize", function () {
-    firstThreeJS.value.width = window.innerWidth;
-    firstThreeJS.value.height = window.innerHeight;
+    firstDemo.clear();
     nextTick(() => {
-      firstDemo.clear();
-      firstDemo.render();
+      firstDemo.render(window.innerWidth, window.innerHeight);
     });
   });
 }
@@ -112,7 +129,7 @@ onMounted(async () => {
   listeningToScreenSize(firstDemo);
 
   document.documentElement.onclick = function () {
-    console.log(1111111);
+    console.log("click");
   };
 });
 </script>
